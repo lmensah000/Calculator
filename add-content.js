8874890d3339c4aca587a8a6e4a692ef5e966b7a
Lawrence Mensah <lmensah000@gmail.com>
2019-06-17 14:41:54 -0400

    First commit Project calculator

---
+++ add-content.js
@@ -0,0 +1,7 @@
+window.onload = function() {
+/* entering input values into two boxes and clicking "=="string and "=="value
+to find true or false result
+*/
+  function
+var firstValue= document.getElementById("firstValue");
+var secondValue= document.getElementById("secondValue");
